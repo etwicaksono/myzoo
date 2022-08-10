f775256ee1a344f9691c1c82f62693c15b7e00ad
Eko Teguh Wicaksono <ekoteguhwicaksono@gmail.com>
2022-08-10 17:12:15 +0700

    Hide action bar on splash screen

--- app/src/main/java/com/etwicaksono/myzoo/ui/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/etwicaksono/myzoo/ui/activity/SplashScreenActivity.kt
@@ -1,21 +1,24 @@
 package com.etwicaksono.myzoo.ui.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.etwicaksono.myzoo.R
 import com.etwicaksono.myzoo.ui.activity.home.HomeActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
 
         Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this@SplashScreenActivity, HomeActivity::class.java))
             finish()
-        },2000)
+        }, 2000)
     }
 }
