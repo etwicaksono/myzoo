4ea7877d1310b0085a0e664f8b0142f798dad11d
Eko Teguh Wicaksono <ekoteguhwicaksono@gmail.com>
2022-08-12 04:14:43 +0700

    change layout

--- app/src/main/java/com/etwicaksono/myzoo/ui/activity/detail/DetailActivity.kt
+++ app/src/main/java/com/etwicaksono/myzoo/ui/activity/detail/DetailActivity.kt
@@ -1,17 +1,42 @@
 package com.etwicaksono.myzoo.ui.activity.detail
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.etwicaksono.myzoo.R
 import com.etwicaksono.myzoo.databinding.ActivityDetailBinding
+import com.etwicaksono.myzoo.responses.ResponseAnimal
 
 class DetailActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityDetailBinding
+    private lateinit var binding: ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityDetailBinding.inflate(layoutInflater)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val animalList = intent.getSerializableExtra("animalList") as List<ResponseAnimal>?
+        val position = intent.getIntExtra("position", 0)
+        val animal = animalList?.get(position)
+
+        binding.apply {
+            Glide.with(this@DetailActivity)
+                .load(animal?.imageLink)
+                .placeholder(R.drawable.default_image)
+                .into(ivAnimal)
+
+            tvName.text = animal?.name
+            tvLatinName.text = animal?.latinName
+            tvType.text = animal?.animalType
+            tvActiveTime.text = animal?.activeTime
+            tvLengthMin.text = "${animal?.lengthMin} ft"
+            tvLengthMax.text = "${animal?.lengthMax} ft"
+            tvWeightMin.text = "${animal?.weightMin} lbs"
+            tvWeightMax.text = "${animal?.weightMax} lbs"
+            tvLifespan.text = animal?.lifespan + if(animal?.lifespan?.toInt()?.equals(1) == true) "years" else "year"
+            tvHabitat.text = animal?.habitat
+            tvDiet.text = animal?.diet
+            tvGeoRange.text = animal?.geoRange
+        }
     }
 }
