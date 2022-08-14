212c7d87c88b6b65e4d9cecdd511c18dddf19833
Eko Teguh Wicaksono <ekoteguhwicaksono@gmail.com>
2022-08-14 09:01:56 +0700

    Update HomeActivity.kt

--- app/src/main/java/com/etwicaksono/myzoo/ui/activity/home/HomeActivity.kt
+++ app/src/main/java/com/etwicaksono/myzoo/ui/activity/home/HomeActivity.kt
@@ -3,62 +3,41 @@ package com.etwicaksono.myzoo.ui.activity.home
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.core.view.isVisible
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
+import com.etwicaksono.myzoo.MyViewModelFactory
+import com.etwicaksono.myzoo.api.ApiConfig
 import com.etwicaksono.myzoo.databinding.ActivityHomeBinding
+import com.etwicaksono.myzoo.repository.MainRepository
+import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
     private lateinit var animalPagerAdapter: AnimalPagerAdapter
-    private val viewModel: AnimalsListViewModel by viewModels()
-    private var isLoading = false
+    private lateinit var viewModel: AnimalsListViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val layoutManager = LinearLayoutManager(this)
-        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
-        animalPagerAdapter = AnimalPagerAdapter()
-        binding.rvAnimals.apply {
-            this.adapter = animalPagerAdapter
-            this.layoutManager = layoutManager
-            addItemDecoration(itemDecoration)
+        val apiService = ApiConfig.getApiService()
+        val mainRepository = MainRepository(apiService)
+        binding.rvAnimals.adapter = animalPagerAdapter
 
-            addOnScrollListener(object : RecyclerView.OnScrollListener() {
-                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                    val countItem = layoutManager.itemCount
-                    val lastVisiblePosition = layoutManager.findLastVisibleItemPosition()
-                    val isLastPosition = countItem.minus(1) == lastVisiblePosition
-
-                    if (isLastPosition && !isLoading && dy > 0) {
-                        viewModel.addMore()
-                    }
-                }
-            })
-        }
+        viewModel = ViewModelProvider(
+            this,
+            MyViewModelFactory(mainRepository)
+        )[AnimalsListViewModel::class.java]
 
         viewModel.apply {
-            init()
 
-            listAnimals.observe(this@HomeActivity) {
-                if (it != null && it.isNotEmpty()) animalPagerAdapter.setAnimalsListData(
-                    it
-                )
-            }
-
-            mainLoading.observe(this@HomeActivity) {
-                binding.centerProgressBar.visibility = if (it) View.VISIBLE else View.INVISIBLE
-                isLoading = it
-            }
-            refreshLoading.observe(this@HomeActivity) {
-                binding.bottomProgressBar.visibility = if (it) View.VISIBLE else View.INVISIBLE
-                isLoading = it
+            errorMessage.observe(this@HomeActivity) {
+                Toast.makeText(this@HomeActivity, it, Toast.LENGTH_SHORT).show()
             }
 
             hasInternet(this@HomeActivity).observe(this@HomeActivity) {
@@ -66,6 +45,36 @@ class HomeActivity : AppCompatActivity() {
                     Toast.makeText(this@HomeActivity, "Internet unavailable", Toast.LENGTH_LONG)
                         .show()
                     binding.centerProgressBar.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+        animalPagerAdapter.addLoadStateListener { loadState ->
+//                        show empty list
+            if (loadState.refresh is LoadState.Loading ||
+                loadState.append is LoadState.Loading
+            ) {
+                binding.centerProgressBar.isVisible = true
+            } else {
+                binding.centerProgressBar.isVisible = false
+//                if we have an error, show a toast
+                val errorState = when {
+                    loadState.append is LoadState.Error -> loadState.append as LoadState.Error
+                    loadState.prepend is LoadState.Error -> loadState.prepend as LoadState.Error
+                    loadState.refresh is LoadState.Error -> loadState.refresh as LoadState.Error
+                    else -> null
+                }
+                errorState?.let {
+                    Toast.makeText(this, it.error.toString(), Toast.LENGTH_LONG).show()
+                }
+            }
+
+        }
+
+        lifecycleScope.launch {
+            viewModel.getAnimalList().observe(this@HomeActivity) {
+                it?.let {
+                    animalPagerAdapter.submitData(lifecycle, it)
                 }
             }
         }
