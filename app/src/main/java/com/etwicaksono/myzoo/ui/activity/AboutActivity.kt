9336ca99e50b4ae3a514dbe03c10ec7c02bac6d1
Eko Teguh Wicaksono <ekoteguhwicaksono@gmail.com>
2022-08-14 12:55:19 +0700

    Add back button

--- app/src/main/java/com/etwicaksono/myzoo/ui/activity/AboutActivity.kt
+++ app/src/main/java/com/etwicaksono/myzoo/ui/activity/AboutActivity.kt
@@ -1,12 +1,21 @@
 package com.etwicaksono.myzoo.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.etwicaksono.myzoo.R
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val actionBar = supportActionBar
+        actionBar?.title = "About"
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
