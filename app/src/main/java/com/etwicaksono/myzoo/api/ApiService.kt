96c4779f72988e0edcf474b5297f1cc5df40010d
Eko Teguh Wicaksono <ekoteguhwicaksono@gmail.com>
2022-08-14 07:51:42 +0700

    Add AnimalPagingSource.kt

--- app/src/main/java/com/etwicaksono/myzoo/api/ApiService.kt
+++ app/src/main/java/com/etwicaksono/myzoo/api/ApiService.kt
@@ -2,9 +2,10 @@ package com.etwicaksono.myzoo.api
 
 import com.etwicaksono.myzoo.responses.ResponseAnimal
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
     @GET("animals/rand/10")
-    fun getAllAnimals(): Call<MutableList<ResponseAnimal>>
+    suspend fun getAllAnimals(): Response<List<ResponseAnimal>>
 }
---
+++ app/src/main/java/com/etwicaksono/myzoo/paging/AnimalPagingSource.kt
@@ -0,0 +1,25 @@
+package com.etwicaksono.myzoo.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.etwicaksono.myzoo.api.ApiService
+import com.etwicaksono.myzoo.responses.ResponseAnimal
+
+class AnimalPagingSource(private val apiService: ApiService) : PagingSource<Int, ResponseAnimal>() {
+    override fun getRefreshKey(state: PagingState<Int, ResponseAnimal>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, ResponseAnimal> {
+        return try {
+            val position = params.key?:1
+            val response =apiService.getAllAnimals()
+            LoadResult.Page(data = response.body()!!, prevKey = if(position==1)null else position-1, nextKey = position+1)
+        }catch (e:Exception){
+            LoadResult.Error(e)
+        }
+    }
+}
