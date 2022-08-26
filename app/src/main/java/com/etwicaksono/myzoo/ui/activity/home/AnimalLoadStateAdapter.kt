7841e7b64fb4dc9a6655614d1d952208d3c63064
Eko Teguh Wicaksono <echotechno007@gmail.com>
2022-08-26 07:21:45 +0700

    Setup AnimalLoadStateAdapter

--- app/src/main/java/com/etwicaksono/myzoo/ui/activity/home/AnimalLoadStateAdapter.kt
+++ app/src/main/java/com/etwicaksono/myzoo/ui/activity/home/AnimalLoadStateAdapter.kt
@@ -2,13 +2,35 @@ package com.etwicaksono.myzoo.ui.activity.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.etwicaksono.myzoo.R
+import com.etwicaksono.myzoo.databinding.ItemRowLoadingBinding
 
-class AnimalLoadStateAdapter(private val retry:()->Unit):LoadStateAdapter<AnimalLoadStateAdapter.LoadStateViewHolder>() {
-    class LoadStateViewHolder(parent: ViewGroup,retry: () -> Unit):RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate()) {
+class AnimalLoadStateAdapter(private val retry: () -> Unit) :
+    LoadStateAdapter<AnimalLoadStateAdapter.LoadStateViewHolder>() {
+    class LoadStateViewHolder(parent: ViewGroup, retry: () -> Unit) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.item_row_loading, parent, false
+        )
+    ) {
+        private val binding = ItemRowLoadingBinding.bind(itemView)
+        private val progressAnimation = binding.progressCircular
+        private val message = binding.progressMessage
+        private val retry = binding.buttonRetry.also { it.setOnClickListener { retry() } }
 
+        fun bind(loadState: LoadState) {
+            if (loadState is LoadState.Error) {
+                message.text = loadState.error.localizedMessage
+            }
+
+            progressAnimation.isVisible = loadState is LoadState.Loading
+            retry.isVisible = loadState is LoadState.Error
+            message.isVisible = loadState is LoadState.Error
+        }
     }
 
     override fun onBindViewHolder(holder: LoadStateViewHolder, loadState: LoadState) {
